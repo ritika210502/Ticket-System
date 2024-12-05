549096537f06ffa9c07bd0f04110e5e7ca7e9e70
Ritika Chourasia <93917787+ritika210502@users.noreply.github.com>
2024-12-05 07:55:34 +0530

    Update settings.py

--- ticketing_system/settings.py
+++ ticketing_system/settings.py
@@ -29,7 +29,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ritikachourasia21@gmail.com'  # Replace with your Gmail address
+EMAIL_HOST_USER = 'example@gmail.com'  # Replace with your Gmail address
 EMAIL_HOST_PASSWORD = 'jdxj qpko iiyh xtkf'  # Replace with your Gmail app password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
